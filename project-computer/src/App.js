f8a89d75cca4120c35d431b8f090ec90626e2bcb
nguyenwanghuy <nguyenquanghuy04012001@gmail.com>
2023-06-17 22:42:49 +0700

    detail

--- project-computer/src/App.js
+++ project-computer/src/App.js
@@ -4,13 +4,16 @@ import Header from './Layout/Header';
 import Nav from './Layout/Nav';
 import Footer from './Layout/Footer';
 import style from './Assets/style.css'
-
+// import Homepage from './pages/Homepage/Homepage';
+import detail from './Assets/detail.css';
+import Detail from './Details/Detail.jsx';
 function App() {
   return (
     <div className="App">
-          <Nav />
-          <Header></Header>
-          <Footer></Footer>
+         
+        <Detail/>
+
+
     </div>
   );
 }
